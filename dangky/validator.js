e3842474d04e8082497fbf809df95dcb122e7b8f
NguyenDuyTan18 <nguyenduytanst2020@gmail.com>
2024-11-02 00:03:55 +0700

    4 para  commit

--- dangky/validator.js
+++ dangky/validator.js
@@ -1,11 +1,88 @@
+let form1 = document.getElementById('form-1')
+
 function Validator (options) {
 
+    //Hàm thự hiện validate
+    function validate(inputElement, rule) {
+        
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var errorMessage = rule.test(inputElement.value);
+                  
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+    }
+
+    //lấy element của form cần validate
+    var formElement = document.querySelector(options.form);
+
+    if (formElement) {
+
+        options.rules.forEach(function(rule){
+            
+            var inputElement = formElement.querySelector(rule.selector);
+         
+            if (inputElement) {
+                // xử lý trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+
+                //xử lý mỗi khi người dùng nhập
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid');
+                }
+            }
+
+        }) ;
+    }
+
 }
 
-Validator.isRequired = function() {
-
+Validator.isRequired = function(selector) {
+     return {
+        selector: selector, 
+        test: function(value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+        }
+     };
 }
 
-Validator.isEmail = function() {
-    
+Validator.isEmail = function(selector) {
+    return {
+        selector: selector, 
+        test: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Vui lòng nhập đúng định dạng email';
+        }
+     };
 }
+
+Validator.minLength = function(selector, min) {
+    return {
+        selector: selector, 
+        test: function(value) {
+            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự`;
+        }
+     };
+} 
+
+Validator.isConfirmed = function (selector, getConfirmValue) {
+    return{
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue () ? undefined : 'Giá trị nhập vào không chính xác';
+        }
+    }
+}
+
+form1.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    alert("Bạn đã đăng ký thành công")
+})
